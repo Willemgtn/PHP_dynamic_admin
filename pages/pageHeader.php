74293a1602b7203d21f7231925cbbb7831b34b46
Willemgtn <a.w.cerqueira@hotmail.com>
2022-08-31 17:03:56 -0300

    front-end public page

---
+++ pages/pageHeader.php
@@ -0,0 +1,20 @@
+<header class="pageHeader">
+    <div class="center">
+        <div class="logo f-left"><a href="./"><?php echo $sql['logotitle'] ?></a></div>
+        <nav class="desktop f-right">
+            <div class="menu-btn">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li><a href="./?url=sobre">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="./?url=contact">Contatos</a></li>
+                <li><a href="./painel/">Admin</a></li>
+            </ul>
+
+        </nav>
+    </div>
+    <div class="clear"></div>
+</header>
