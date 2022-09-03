d3e9875d9dea37ca6f5dd4135420af32bc784608
Willemgtn <a.w.cerqueira@hotmail.com>
2022-09-03 15:31:50 -0300

    debug

--- classes/autoload.php
+++ classes/autoload.php
@@ -2,17 +2,12 @@
 
 
 spl_autoload_register(function ($class) {
+    $debug = false;
     // project-specific namespace prefix
     $prefix = '';
 
     // For backwards compatibility
     $customBaseDir = '';
-    // @todo v6: Remove support for 'FACEBOOK_SDK_V4_SRC_DIR'
-    // if (defined('FACEBOOK_SDK_V4_SRC_DIR')) {
-    //     $customBaseDir = FACEBOOK_SDK_V4_SRC_DIR;
-    // } elseif (defined('FACEBOOK_SDK_SRC_DIR')) {
-    //     $customBaseDir = FACEBOOK_SDK_SRC_DIR;
-    // }
 
     // base directory for the namespace prefix
     $baseDir = $customBaseDir ?: __DIR__ . '/';
@@ -32,24 +27,6 @@ spl_autoload_register(function ($class) {
     // separators with directory separators in the relative class name, append
     // with .php
 
-    // $file1 = rtrim($baseDir, '/') . '/' . str_replace('\\', '/', $relativeClass) . '.php';
-    // $file2 = rtrim($baseDir, '/') . '/../' . str_replace('\\', '/', $relativeClass) . '.php';
-    // $file3 = rtrim($baseDir, '/') . '/../../' . str_replace('\\', '/', $relativeClass) . '.php';
-    // echo "Auth File required: $file1 <br>";
-
-
-
-    // if the file exists, require it
-    // if (file_exists($file1)) {
-    //     require $file1;
-    // } else if (file_exists($file2)){
-    //     require $file2;
-    // } else if (file_exists($file3)){
-    //     require $file3;
-    // } else {
-    //     echo "<hr> File could not be found:  $baseDir $relativeClass.php <hr>";
-    // }
-
 
     $file = str_replace('\\', '/', $relativeClass) . '.php';
     // $baseDir = rtrim($baseDir, '/');
@@ -60,6 +37,12 @@ spl_autoload_register(function ($class) {
     } else if (file_exists($baseDir . '../../' . $file)) {
         require($baseDir . '../../' . $file);
     } else {
-        echo "<hr> File could not be found:  $baseDir $relativeClass.php <hr>";
+        // echo "<hr> File could not be found:  $baseDir $relativeClass.php <hr>";
+        if ($debug) {
+            echo "<hr> File could not be found:  $baseDir $relativeClass.php <hr>";
+        }
+    }
+    if ($debug) {
+        echo "<hr> classes/Autoload::   baseDir: $baseDir, RelativeClass: $relativeClass <hr>";
     }
 });
