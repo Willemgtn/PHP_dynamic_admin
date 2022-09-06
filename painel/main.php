00f332f7361c24afb5186ad3c239d07f92c431c8
Willemgtn <a.w.cerqueira@hotmail.com>
2022-09-05 23:45:26 -0300

    loadJS at specific defined pages

--- painel/main.php
+++ painel/main.php
@@ -2,6 +2,7 @@
 if (isset($_GET['logout'])) {
     Painel::logout();
 }
+
 ?>
 
 
@@ -13,6 +14,13 @@ if (isset($_GET['logout'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel de controle</title>
+    <!-- External instance -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script> -->
+    <!-- End External instance -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"> -->
+    <!-- END bootstrap icons -->
+    <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="../fontawesome/css/all.css">
     <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/newform.css">
@@ -208,11 +216,16 @@ if (isset($_GET['logout'])) {
     <script src="https://cdn.tiny.cloud/1/<?php echo TINYMCE_API_KEY ?>/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <!-- <script src="../js/tinymce_6.1.2-8.js"></script> -->
     <!-- <script src="../js/tinymce/tinymce.min.js"></script> -->
-    <script src="https://malsup.github.io/jquery.form.js"></script>
-    <script src="../js/jquery.mask.js"></script>
+    <!-- <script src="https://malsup.github.io/jquery.form.js"></script> -->
+    <!-- <script src="../js/jquery.mask.js"></script>
     <script src="./js/helperMask.js"></script>
-    <script src="./js/ajax.js"></script>
+    <script src="./js/ajax.js"></script> -->
 
+    <?php
+    // Loading page specific js scripts
+    Painel::loadJS(['jquery.mask.js', 'helperMask.js', 'jquery.form.js', 'ajax.js'], 'addClient', true);
+
+    ?>
 
     <script>
         tinymce.init({
