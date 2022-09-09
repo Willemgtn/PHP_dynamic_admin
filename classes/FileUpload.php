86dfa55d4152a3a36348bc9b00975d9d7c76b801
Willemgtn <a.w.cerqueira@hotmail.com>
2022-09-09 01:04:30 -0300

    Estoque with multiple (ph)fotos

--- classes/FileUpload.php
+++ classes/FileUpload.php
@@ -78,6 +78,80 @@ class FileUpload
             }
         }
     }
+    static function arrImageValidate(array $imgArray, $debug = false)
+    {
+        $uploadOk = 1;
+        if ($debug) {
+            print_r($imgArray);
+            echo "<br>";
+        }
+        $target_file = self::$target_dir . basename($imgArray["name"]);
+
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+
+        @$check = getimagesize($imgArray["tmp_name"]);
+        if ($check !== false) {
+            if ($debug) echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            if ($debug) echo "File is not an image.";
+            $uploadOk = 0;
+            return false;
+        }
+        if ($debug) {
+            echo "<pre>";
+            print_r($_FILES);
+            echo "</pre>";
+        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            if ($debug) echo "Sorry, file already exists.";
+            $uploadOk = 0;
+            return false;
+        }
+
+        // Check file size
+        if (intval($imgArray["size"]) / 1024 > 1500) {
+            if ($debug) echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+            return false;
+        }
+
+        // Allow certain file formats
+        if (
+            $imageFileType != "jpg" &&
+            $imageFileType != "png" &&
+            $imageFileType != "jpeg" &&
+            $imageFileType != "webp" &&
+            $imageFileType != "gif"
+        ) {
+            if ($debug) echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+            return false;
+        }
+        return $uploadOk;
+    }
+
+    static function arrImageUpload(array $imgArray, $debug = false)
+    {
+        $uniqueFileName = uniqid() . '.' . strtolower(pathinfo($imgArray["name"], PATHINFO_EXTENSION));
+        $target_file = self::$target_dir . basename($uniqueFileName);
+        // $target_file = self::$target_dir . basename($imgArray["name"]);
+
+        if (self::arrImageValidate($imgArray) == 0) {
+            if ($debug)  echo "Sorry, your file was not uploaded.";
+        } else {
+            if (move_uploaded_file($imgArray["tmp_name"], $target_file)) {
+                if ($debug) echo "The file " . htmlspecialchars(basename($imgArray["name"])) . " has been uploaded.";
+                return $uniqueFileName;
+            } else {
+                if ($debug) echo "Sorry, there was an error uploading your file.";
+                return false;
+            }
+        }
+    }
     static function deleteFIle($file)
     {
         @unlink(self::$target_dir . $file);
---
+++ painel/pages/estoque.php
@@ -0,0 +1,95 @@
+<?php
+$pageTable = 'tb_admin.clientes';
+$pageTableFin = 'tb_admin.clientes-financeiro';
+function pageUrl($next = null)
+{
+    $baseUrl = './clientes';
+    // echo $baseUrl . $next;
+    // return $baseUrl . $next;
+    return $next ? $baseUrl . $next : $baseUrl;
+}
+$maxItemsPerPage = 6;
+
+?>
+
+<?php //if (isset($_GET['add'])) {
+?>
+<section id="" class="new-form">
+    <h2>
+        <i class="fa-solid fa-plus"></i>
+        Cadastrar Produto
+    </h2>
+    <?php
+    if (isset($_POST['submit'])) {
+        Painel::htmlPopUp('ok', 'O produto foi cadastrado com sucesso');
+        $nome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
+        $largura = $_POST['largura'];
+        $altura = $_POST['altura'];
+        $comprimento = $_POST['comprimento'];
+        $peso = $_POST['peso'];
+        $quantidade = $_POST['quantidade'];
+
+        $ok = true;
+
+        // $images;
+        if ($_FILES['img']['name'][0] != '') {
+            for ($i = 0; $i < count($_FILES['img']['name']); $i++) {
+                $imagem = [
+                    'name' => $_FILES['img']['name'][$i],
+                    'type' => $_FILES['img']['type'][$i],
+                    'tmp_name' => $_FILES['img']['tmp_name'][$i],
+                    'error' => $_FILES['img']['error'][$i],
+                    'size' => $_FILES['img']['size'][$i],
+                ];
+                if (FileUpload::arrImageValidate($imagem)) {
+                    $imagens[] = $imagem;
+                }
+            }
+            if ($imagens) {
+                Painel::htmlPopUp('ok', 'continue');
+            }
+            foreach ($imagens as $key => $value) {
+                // $imagens[$key]['upload_name'] = FileUpload::arrImageUpload($value);
+            }
+            echo "<pre>";
+            print_r($imagens);
+            echo "</pre>";
+        } else {
+            Painel::htmlPopUp('error', 'Por favor selecione alguma foto');
+        }
+    }
+    ?>
+    <form class="" action="" method="post" enctype="multipart/form-data" resetar>
+        <label for="nome">Nome do produto:</label>
+        <input type="text" name="nome" id="" placeholder="Nome do Cliente/Empresa">
+        <label for="descricao">Descrição do produto:</label>
+        <textarea class="tiny" name="descricao" id="" cols="" rows=""></textarea>
+        <!-- <input type="descricao" name="text" id="" placeholder="Descricao do produto"> -->
+        <!-- <div class="form-row" style="display: flex;"> -->
+        <label for="largura">Largura:</label>
+        <input type="number" name="largura" id="" min="0" max="900" step="5" value="0">
+        <label for="altura">Altura:</label>
+        <input type="number" name="altura" id="" min="0" max="900" step="5" value="0">
+        <label for="comprimento">comprimento:</label>
+        <input type="number" name="comprimento" id="" min="0" max="900" step="5" value="0">
+        <label for="peso">Peso:</label>
+        <input type="number" name="peso" id="" min="0" max="900" step="5" value="0">
+        <label for="quantidade">Quantidade:</label>
+        <input type="number" name="quantidade" id="" min="0" max="900" step="5" value="0">
+        <!-- </div> -->
+        <!-- <label for="tipo_cliente">Tipo:</label>
+        <select name="tipo_cliente" id="">
+            <option value="fisico">Fisico</option>
+            <option value="juridico">Juridico</option>
+        </select>
+        <label for="inscricao">CPF: </label>
+        <input type="text" name="cpf" id="inscricao" placeholder="000.000.000-00"> -->
+        <label for="img">Selecione as imagens:</label>
+        <input multiple type="file" name="img[]" id="">
+        <input type="submit" name="submit" value="Cadastrar">
+
+    </form>
+</section>
+<?php // } 
+?>
