f35d7c444d486dfeb46df4a7568d3eacb867499b
Willemgtn <a.w.cerqueira@hotmail.com>
2022-09-09 14:03:43 -0300

    list products design

--- painel/main.php
+++ painel/main.php
@@ -80,7 +80,7 @@ if (isset($_GET['logout'])) {
                 ['permission' => '1', 'type' => 'title', 'content' => 'Internal'],
                 ['permission' => '1', 'type' => 'link', 'link' => './clientes', 'content' => 'Clientes Gestão'],
                 ['permission' => '1', 'type' => 'link', 'link' => './clientesFinanceiro', 'content' => 'Clientes Financeiro'],
-                // ['permission' => '1', 'type' => 'link', 'link' => '?url=addSlide', 'content' => 'Cadastro Slides'],
+                ['permission' => '1', 'type' => 'link', 'link' => './estoque', 'content' => 'Controle de estoque'],
 
                 // ['permission' => '0', 'type' => 'title', 'content' => 'Gestão'],
                 // ['permission' => '0', 'type' => 'link', 'link' => '?url=listDepo', 'content' => 'Listar Depoimento'],
@@ -234,6 +234,7 @@ if (isset($_GET['logout'])) {
     Painel::loadJS(['jquery.mask.js', 'helperMask.js', 'jquery.maskMoney.js', 'jquery.zebra.dataPicker.js', 'jquery.form.js', 'ajax.js'], 'clientes', true);
     // Painel::loadJS(['jquery.mask.js', 'helperMask.js', 'jquery.form.js', 'ajax.js', 'cliente.js'], 'clients/add', true);
 
+    Painel::loadJS(['jquery.mask.js', 'helperMask.js', 'jquery.maskMoney.js', 'jquery.zebra.dataPicker.js', 'jquery.form.js', 'ajax.js'], 'estoque', true);
     ?>
 
     <script>
--- painel/pages/estoque.php
+++ painel/pages/estoque.php
@@ -3,7 +3,7 @@ $pageTable = 'tb_admin.estoque';
 $pageTableImg = 'tb_admin.estoque_imagens';
 function pageUrl($next = null)
 {
-    $baseUrl = './clientes';
+    $baseUrl = './estoque';
     // echo $baseUrl . $next;
     // return $baseUrl . $next;
     return $next ? $baseUrl . $next : $baseUrl;
@@ -82,20 +82,14 @@ $maxItemsPerPage = 6;
         <input type="number" name="largura" id="" min="0" max="900" step="5" value="0">
         <label for="altura">Altura:</label>
         <input type="number" name="altura" id="" min="0" max="900" step="5" value="0">
-        <label for="comprimento">comprimento:</label>
+        <label for="comprimento">Comprimento:</label>
         <input type="number" name="comprimento" id="" min="0" max="900" step="5" value="0">
         <label for="peso">Peso:</label>
         <input type="number" name="peso" id="" min="0" max="900" step="5" value="0">
         <label for="quantidade">Quantidade:</label>
         <input type="number" name="quantidade" id="" min="0" max="900" step="5" value="0">
         <!-- </div> -->
-        <!-- <label for="tipo_cliente">Tipo:</label>
-        <select name="tipo_cliente" id="">
-            <option value="fisico">Fisico</option>
-            <option value="juridico">Juridico</option>
-        </select>
-        <label for="inscricao">CPF: </label>
-        <input type="text" name="cpf" id="inscricao" placeholder="000.000.000-00"> -->
+
         <label for="img">Selecione as imagens:</label>
         <input multiple type="file" name="img[]" id="">
         <input type="submit" name="submit" value="Cadastrar">
@@ -104,3 +98,114 @@ $maxItemsPerPage = 6;
 </section>
 <?php // } 
 ?>
+<section id="" class="">
+    <h3>
+        <i class="fa-solid fa-address-card"></i>
+        Produtos
+        <a style="float: right;" class="btn green" href="<?php echo pageUrl('?add'); ?>"><i class="fa-solid fa-plus"></i>Add New </a>
+    </h3>
+    <br>
+    <div class="w100">
+        <form action="" method="post" class="search d-flex" style="margin:20px 0;">
+            <!-- <h4>Realize uma busca</h4> -->
+            <input type="text" name="busca" id="" style="flex-grow:1;" placeholder="Realize uma busca pelo: nome ou descricao do produto">
+            <input type="submit" value="Buscar">
+        </form>
+    </div>
+
+    <?php
+    // -----> SQL <-----
+    if (isset($_POST['busca'])) {
+        $busca = $_POST['busca'];
+        $searchQuery = "WHERE nome LIKE '%$busca%' OR descricao LIKE '%$busca%' ";
+    }
+    $searchQuery = $searchQuery ?? null;
+    // -----> SQL <-----
+    $produtos = Sql::connect()->prepare("SELECT * from `$pageTable`  $searchQuery");
+    $produtos->execute();
+    $produtos = $produtos->fetchAll();
+    // 
+    if (isset($_POST['busca'])) {
+        echo '<div class="busca-resultado"><p>Foram encontrados <b>' . count($produtos) . '</b> resultado(s)</p></div>';
+    }
+
+    ?>
+
+
+    <!-- SQL fetchAll foreach template -->
+    <div class="cardsWrapper">
+        <!-- template -->
+        <?php
+
+        foreach ($produtos as $value) {
+
+
+            // Fake demo filling
+            // $value = ['id' => '0', 'nome' => 'Adriaan', 'email' => 'hotmail.gmail.com', 'tipo' => 'fisico2', 'inscricao' => '000.000.000-00'];
+            // for ($i = 0; $i < 6; $i++) {
+
+        ?>
+
+            <div class="roundedBorders">
+                <?php
+                $sql = Sql::connect()->prepare("SELECT imagem FROM `$pageTableImg` WHERE produto_id = $value[id]");
+                $sql->execute();
+                $sql = $sql->fetchAll();
+                // print_r($sql);
+                foreach ($sql as $key => $img) {
+                    echo "<img src='./uploads/$img[imagem]'>";
+                }
+                ?>
+                <hr>
+                <ul>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Nome:</strong>
+                        <!-- PHP -->
+                        <?php echo $value['nome'] ?>
+                    </li>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Descrição:</strong>
+                        <!-- PHP -->
+                        <?php echo $value['descricao'] ?>
+                    </li>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Largura:</strong>
+                        <!-- PHP -->
+                        <?php echo ucfirst($value['largura']) ?>
+                    </li>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Altura:</strong>
+                        <!-- PHP -->
+                        <?php echo ucfirst($value['altura']) ?>
+                    </li>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Comprimento:</strong>
+                        <!-- PHP -->
+                        <?php echo ucfirst($value['comprimento']) ?>
+                    </li>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Peso:</strong>
+                        <!-- PHP -->
+                        <?php echo ucfirst($value['peso']) ?>
+                    </li>
+                    <li>
+                        <strong>
+                            <label for="quantidade">Quantidade:</label>
+                        </strong>
+                        <form action="" class="search d-flex" style="margin:0;">
+                            <input type="number" name="quantidade" id="" value="<?php echo $value['quantidade'] ?>" style="padding-left:10px;" min="0" max="900" step="5">
+                            <input type="submit" value="Atualizar" class="btn">
+                        </form>
+                    </li>
+                    <hr style="margin:5px -10px;">
+                    <div class="d-flex" style="margin:10px 0;">
+                        <a class="btn edit" href="<?php echo pageUrl('?edit=' . $value['id']) ?>">edit</a>
+                        <a class="btn red delete" href="<?php echo pageUrl('?delete=') ?>" item_id="<?php echo $value['id'] ?>">delete</a>
+                    </div>
+                </ul>
+
+            </div>
+        <?php } ?>
+
+    </div>
+</section>
