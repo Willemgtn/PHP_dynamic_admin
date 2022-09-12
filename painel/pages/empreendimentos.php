57a8abf541a9f967e455ec03a97397cd57b8e874
Willemgtn <a.w.cerqueira@hotmail.com>
2022-09-12 02:57:55 -0300

    listing

--- painel/pages/empreendimentos.php
+++ painel/pages/empreendimentos.php
@@ -39,8 +39,9 @@ $maxItemsPerPage = 6;
                 } else {
                     $imgName = FileUpload::arrImageUpload($imagem);
                     if (isset($imgName)) {
-                        $sql = Sql::connect()->prepare("INSERT INTO `$pageTable` VALUES  (null,?,?,?,?)");
-                        $sql->execute([$nome, $tipo, $preco, $imgName]);
+                        $lastId = Sql::connect()->query("SELECT id from `$pageTable` ORDER BY id DESC LIMIT 1")->fetch()[0];
+                        $sql = Sql::connect()->prepare("INSERT INTO `$pageTable` VALUES  (null,?,?,?,?,?)");
+                        $sql->execute([$nome, $tipo, $preco, $imgName, $lastId + 1]);
                         Painel::htmlPopUp('ok', 'Empreendimento cadastrado');
                     }
                 }
@@ -70,3 +71,120 @@ $maxItemsPerPage = 6;
 </section>
 <?php // } 
 ?>
+
+<section id="" class="">
+    <h3>
+        <i class="fa-solid fa-address-card"></i>
+        Empreendimentos
+        <a style="float: right;" class="btn green" href="<?php echo pageUrl('?add'); ?>"><i class="fa-solid fa-plus"></i>Add New </a>
+    </h3>
+    <br>
+
+    <?php
+    // $sql = Sql::connect()->prepare("SELECT * FROM `$pageTable` ");
+    // $sql->execute();
+    // if ($sql->rowCount() > 0) {
+    //     Painel::htmlPopUp('warn', "Há empreendimentos em falta no estoque! Clique <a href='./estoque?pendentes'>Aqui</a> para visualiza-los!");
+    // }
+    // Painel::htmlPopUp('warn', "Há empreendimentos em falta no estoque! Clique <a href='./estoque?pendentes'>Aqui</a> para visualiza-los!");
+
+    ?>
+    <div class="w100">
+        <form action="" method="post" class="search d-flex" style="margin:20px 0;">
+            <!-- <h4>Realize uma busca</h4> -->
+            <input type="text" name="busca" id="" style="flex-grow:1;" placeholder="Realize uma busca pelo: nome ou descricao do produto">
+            <input type="submit" value="Buscar">
+        </form>
+    </div>
+
+    <?php
+
+    if (isset($_GET['delete'])) {
+        $delete = (int)$_GET['delete'];
+        if (is_int(intval($_GET['delete']))) {
+            $empreendimento = Sql::connect()->prepare("SELECT id, imagem FROM `$pageTable` WHERE id = ?");
+            $empreendimento->execute([$delete]);
+            // echo $empreendimento->rowCount();
+            if ($empreendimento->rowCount() == 1) {
+                $value = $empreendimento->fetch();
+                @unlink('./uploads/' . $value['imagem']);
+                $empreendimento = Sql::connect()->prepare("DELETE FROM `$pageTable` WHERE id = ?");
+                $empreendimento->execute([$delete]);
+                Painel::htmlPopUp('ok', 'empreendimento and image were deleted.');
+                // }
+
+            }
+            // print_r($empreendimentos->fetch());
+        } else {
+            Painel::htmlPopUp('error', 'Sql injection?');
+        }
+    }
+    // -----> SQL <-----
+    if (isset($_POST['busca'])) {
+        $busca = $_POST['busca'];
+        $searchQuery = "WHERE (nome LIKE '%$busca%' ) ";
+    }
+    $searchQuery = $searchQuery ?? null;
+    // -----> SQL <-----
+    $empreendimentos = Sql::connect()->prepare("SELECT * from `$pageTable`  $searchQuery");
+    $empreendimentos->execute();
+    $empreendimentos = $empreendimentos->fetchAll();
+    // 
+    if (isset($_POST['busca'])) {
+        echo '<div class="busca-resultado"><p>Foram encontrados <b>' . count($empreendimentos) . '</b> resultado(s)</p></div>';
+    }
+
+
+
+    ?>
+
+
+    <!-- SQL fetchAll foreach template -->
+    <div class="cardsWrapper">
+        <!-- template -->
+        <?php
+        foreach ($empreendimentos as $value) {
+        ?>
+
+            <div class="roundedBorders">
+                <?php
+                // $sql = Sql::connect()->prepare("SELECT imagem FROM `$pageTableImg` WHERE produto_id = $value[id]");
+                // $sql->execute();
+                // $sql = $sql->fetchAll();
+                // // print_r($sql);
+                // foreach ($sql as $key => $img) {
+                //     echo "<img src='./uploads/$img[imagem]'>";
+                // }
+                echo "<img src='./uploads/$value[imagem]'>";
+                ?>
+                <hr>
+                <ul>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Nome:</strong>
+                        <!-- PHP -->
+                        <?php echo $value['nome'] ?>
+                    </li>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Tipo:</strong>
+                        <!-- PHP -->
+                        <?php echo $value['tipo'] ?>
+                    </li>
+                    <li><i class="fa-solid fa-pencil"></i>
+                        <strong>Preço:</strong>
+                        <!-- PHP -->
+                        <?php echo ucfirst($value['preco']) ?>
+                    </li>
+
+
+                    <hr style="margin:5px -10px;">
+                    <div class="d-flex" style="margin:10px 0;">
+                        <a class="btn edit" href="<?php echo pageUrl('?edit=' . $value['id']) ?>">edit</a>
+                        <a class="btn red delete" href="<?php echo pageUrl('?delete=' . $value['id']) ?>" item_id="<?php echo $value['id'] ?>">delete</a>
+                    </div>
+                </ul>
+
+            </div>
+        <?php } ?>
+
+    </div>
+</section>
